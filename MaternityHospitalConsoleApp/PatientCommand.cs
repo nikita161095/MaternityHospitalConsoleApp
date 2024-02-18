4787bbaeeee3766ff9409e18ca31b760f087b9ad
Nikita <nikita161095@live.com>
2024-02-19 01:00:10 +0300

    Добавьте файлы проекта.

---
+++ MaternityHospitalConsoleApp/PatientCommand.cs
@@ -0,0 +1,17 @@
+﻿
+public class PatientCommand
+{
+    public int Id { get; set; }
+    public Name? Name { get; set; }
+    public int Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool Active { get; set; }
+}
+
+public class Name
+{
+    public int Id { get; set; }
+    public string? Use { get; set; }
+    public string? Family { get; set; }
+    public string? Given { get; set; }
+}
---
+++ MaternityHospitalConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool isActive = true;
+while (isActive)
+{
+    Console.WriteLine("0 - Generate 100 patients");
+    Console.WriteLine("1 - Exit");
+	
+    var key = Console.ReadLine();
+    switch (key)
+	{
+		case "0":
+			break;
+		case "1":
+			isActive = false;
+			break;
+        default:
+            Console.WriteLine();
+            Console.WriteLine("Incorrect data");
+			break;
+	}
+    Console.WriteLine();
+}
+Environment.Exit(0);
